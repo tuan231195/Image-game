08fa42dcc39ac887c8caab9eca0e75258efe743a
vdtn359 <vdtn359@uowmail.edu.au>
2015-08-05 11:12:34 +1000

    Second Part

---
+++ app/src/main/java/com/example/tuannguyen/assignment1/view/TileView.java
@@ -0,0 +1,18 @@
+package com.example.tuannguyen.assignment1.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by tuannguyen on 4/08/2015.
+ */
+public class TileView extends LinearLayout {
+    public TileView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TileView(Context context) {
+        super(context);
+    }
+}
