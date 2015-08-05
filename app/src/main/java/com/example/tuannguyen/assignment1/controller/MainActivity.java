2c0b8e1baf53a8e5ad7fd14791d5455ae2f1ea2c
vdtn359 <vdtn359@uowmail.edu.au>
2015-08-05 11:20:38 +1000

    Third Part

--- app/src/main/java/com/example/tuannguyen/assignment1/controller/MainActivity.java
+++ app/src/main/java/com/example/tuannguyen/assignment1/controller/MainActivity.java
@@ -10,18 +10,23 @@ import android.view.MenuItem;
 
 import com.example.tuannguyen.assignment1.R;
 import com.example.tuannguyen.assignment1.model.GameModel;
+import com.example.tuannguyen.assignment1.model.TileData;
+import com.example.tuannguyen.assignment1.view.TileView;
 
 import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private ArrayList<TileView> tileViews;
+    private static final int numTiles = 18;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         ArrayList<Drawable> images = new ArrayList<>();
+
 
         images.add(ResourcesCompat.getDrawable(getResources(), R.drawable.baldhill, null));
         images.add(ResourcesCompat.getDrawable(getResources(), R.drawable.cathedral, null));
@@ -33,7 +38,25 @@ public class MainActivity extends ActionBarActivity {
         images.add(ResourcesCompat.getDrawable(getResources(), R.drawable.lighthouse, null));
         images.add(ResourcesCompat.getDrawable(getResources(), R.drawable.lake, null));
 
-        GameModel model = new GameModel(18, images);
+        GameModel model = new GameModel(numTiles, images);
+
+        tileViews = new ArrayList<>();
+        for (int i = 1; i <= numTiles; i++)
+        {
+            int id = getResources().getIdentifier("tile"+i, "id", getPackageName());
+            TileData data = model.getTileAt(i - 1);
+            TileView tileView = (TileView)findViewById(id);
+            tileView.setImage(data.getImage());
+            tileView.coverImage();
+            tileView.setTileIndex(i - 1);
+            tileView.setTileViewListener(new TileView.TileViewListener() {
+                @Override
+                public void didSelectTile(TileView tileView) {
+                    tileView.revealImage();
+                }
+            });
+        }
+
         Log.d("DEBUG TAG", model.toString());
     }
 
--- app/src/main/java/com/example/tuannguyen/assignment1/view/TileView.java
+++ app/src/main/java/com/example/tuannguyen/assignment1/view/TileView.java
@@ -1,18 +1,93 @@
 package com.example.tuannguyen.assignment1.view;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+
+import com.example.tuannguyen.assignment1.R;
 
 /**
  * Created by tuannguyen on 4/08/2015.
  */
 public class TileView extends LinearLayout {
+    private Drawable mImage;
+    private ImageView mImageView;
+    private int mTileIndex;
+    private TileViewListener mListener;
+
     public TileView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public TileView(Context context) {
         super(context);
+        init();
     }
+
+    public void init()
+    {
+        setPadding(5, 5, 5, 5);
+        mImageView = new ImageView(getContext());
+        mImageView.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+        mImageView.setContentDescription("Press Me");
+        mImageView.setImageResource(R.drawable.question);
+
+        mImageView.setOnTouchListener(new OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (mListener != null)
+                {
+                    mListener.didSelectTile(TileView.this);
+                }
+                return true;
+            }
+        });
+        addView(mImageView);
+    }
+
+    public int getTileIndex() {
+        return mTileIndex;
+    }
+
+    public void setTileIndex(int tileIndex) {
+        this.mTileIndex = tileIndex;
+    }
+
+    public void setImage(Drawable image)
+    {
+        mImage = image;
+    }
+
+
+    public void setTileViewListener(TileViewListener listener)
+    {
+        this.mListener = listener;
+    }
+
+    public void revealImage()
+    {
+        mImageView.setImageDrawable(mImage);
+    }
+
+    public void coverImage()
+    {
+        mImageView.setImageResource(R.drawable.question);
+    }
+
+    public void hideImage()
+    {
+        setVisibility(View.GONE);
+    }
+
+    public interface TileViewListener
+    {
+        void didSelectTile(TileView tileView);
+    }
+
+
 }
